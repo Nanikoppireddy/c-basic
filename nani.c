7991bbf510df6cbdc6daf2ed53ec7e40a72bbcc0
Nanikoppireddy <93192167+Nanikoppireddy@users.noreply.github.com>
2021-10-30 02:29:19 -0400

    Add files via upload

---
+++ nani.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initialization
+	age=21;
+	cgpa=8.0;
+	initial='k';
+	
+	//output
+	printf("age of the student is %d\n",age);
+	printf("cgpa of the student is%f\n",cgpa);
+	printf("surname of the student%c",initial);
+}
